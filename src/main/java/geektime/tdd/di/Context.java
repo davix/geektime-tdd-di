608865b0b89a7ea99122ee14968aac975a6a43a5
Wei Huang <whuang@arubanetworks.com>
2022-06-08 19:05:05 +0800

    Di case 2. end of #13

--- src/main/java/geektime/tdd/di/Context.java
+++ src/main/java/geektime/tdd/di/Context.java
@@ -1,15 +1,28 @@
 package geektime.tdd.di;
 
+import jakarta.inject.Provider;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-    private Map<Class<?>, Object> components = new HashMap<>();
+    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
+
     public <T> void bind(Class<T> type, T instance) {
-        components.put(type, instance);
+        providers.put(type, (Provider<T>) () -> instance);
+    }
+
+    public <T, Impl extends T> void bind(Class<T> type, Class<Impl> implementation) {
+        providers.put(type, (Provider<T>) () -> {
+            try {
+                return (T) implementation.getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public <T> T get(Class<T> type) {
-        return (T) components.get(type);
+        return (T) providers.get(type).get();
     }
 }
--- src/test/java/geektime/tdd/di/ContainerTest.java
+++ src/test/java/geektime/tdd/di/ContainerTest.java
@@ -1,5 +1,6 @@
 package geektime.tdd.di;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +10,19 @@ public class ContainerTest {
     interface Component {
 
     }
+
+    static class ComponentWithDefaultConstructor implements Component {
+        public ComponentWithDefaultConstructor() {
+        }
+    }
+
     @Nested
     public class ComponentConstruction {
         @Test
         public void should_bind_to_a_specific_instance() {
             Context context = new Context();
 
-            Component instance = new Component(){
+            Component instance = new Component() {
             };
             context.bind(Component.class, instance);
 
@@ -24,12 +31,24 @@ public class ContainerTest {
 
         @Nested
         public class ConstructionInjection {
+            @Test
+            public void should_bind_type_to_a_class_with_default_constructor() {
+                Context context = new Context();
+
+                context.bind(Component.class, ComponentWithDefaultConstructor.class);
+
+                Component instance = context.get(Component.class);
+                assertNotNull(instance);
+                assertTrue(instance instanceof ComponentWithDefaultConstructor);
+            }
 
         }
+
         @Nested
         public class FieldInjection {
 
         }
+
         @Nested
         public class MethodInjection {
 
