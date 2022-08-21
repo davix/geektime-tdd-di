63591a8532a025df7b0d28dec0471c7c1bc9cc09
Wei Huang <whuang@arubanetworks.com>
2022-08-21 20:14:04 +0800

    #29 refactor remove providers in ContextConfig

--- src/main/java/geektime/tdd/di/ContextConfig.java
+++ src/main/java/geektime/tdd/di/ContextConfig.java
@@ -4,7 +4,6 @@ import java.lang.annotation.Annotation;
 import java.util.*;
 
 public class ContextConfig {
-    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
     private Map<Component, Provider<?>> components = new HashMap<>();
 
     interface Provider<T> {
@@ -16,7 +15,7 @@ public class ContextConfig {
     }
 
     public <T> void bind(Class<T> type, T instance) {
-        providers.put(type, context -> instance);
+        components.put(new Component(type, null), context -> instance);
     }
 
     public <T> void bind(Class<T> type, T instance, Annotation... qualifiers) {
@@ -25,7 +24,7 @@ public class ContextConfig {
     }
 
     public <T, Impl extends T> void bind(Class<T> type, Class<Impl> implementation) {
-        providers.put(type, new InjectionProvider<>(implementation));
+        components.put(new Component(type, null), new InjectionProvider<>(implementation));
     }
 
     public <T, Impl extends T> void bind(Class<T> type, Class<Impl> implementation, Annotation... qualifiers) {
@@ -37,7 +36,7 @@ public class ContextConfig {
     }
 
     public Context getContext() {
-        providers.keySet().forEach(c -> checkDependencies(c, new Stack<>()));
+        components.keySet().forEach(c -> checkDependencies(c, new Stack<>()));
         return new Context() {
             @Override
             public <T> Optional<T> get(Ref<T> ref) {
@@ -47,23 +46,27 @@ public class ContextConfig {
                 if (ref.isContainer()) {
                     if (ref.getContainer() != jakarta.inject.Provider.class)
                         return Optional.empty();
-                    return (Optional<T>) Optional.ofNullable(providers.get(ref.getComponent()))
+                    return (Optional<T>) Optional.ofNullable(getProvider(ref))
                             .map(provider -> (jakarta.inject.Provider<Object>) () -> provider.get(this));
                 } else
-                    return Optional.ofNullable(providers.get(ref.getComponent())).map(p -> (T) p.get(this));
+                    return Optional.ofNullable(getProvider(ref)).map(p -> (T) p.get(this));
             }
         };
     }
 
-    private void checkDependencies(Class<?> c, Stack<Class<?>> visiting) {
-        for (Context.Ref ref : providers.get(c).getDependencies()) {
-            Class<?> comp = ref.getComponent();
-            if (!providers.containsKey(comp))
-                throw new DependencyNotFoundException(c, comp);
+    private <T> Provider<?> getProvider(Context.Ref<T> ref) {
+        return components.get(new Component(ref.getComponent(), ref.getQualifier()));
+    }
+
+    private void checkDependencies(Component c, Stack<Class<?>> visiting) {
+        for (Context.Ref ref : components.get(c).getDependencies()) {
+            Component comp = new Component(ref.getComponent(), ref.getQualifier());
+            if (!components.containsKey(comp))
+                throw new DependencyNotFoundException(c.type(), ref.getComponent());
             if (!ref.isContainer()) {
-                if (visiting.contains(comp))
+                if (visiting.contains(ref.getComponent()))
                     throw new CyclicDependenciesFound(visiting);
-                visiting.push(comp);
+                visiting.push(ref.getComponent());
                 checkDependencies(comp, visiting);
                 visiting.pop();
             }
